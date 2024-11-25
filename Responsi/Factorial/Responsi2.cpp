4ce28d5d83327e2ab4eafe2806d17d17c3d8c5dc
ReKrieg <142876867+ReKrieg@users.noreply.github.com>
2024-11-25 16:41:08 +0700

    Add files via upload

---
+++ Responsi/Factorial/Responsi2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num;
+    int factorial = 1;
+    cout << "Input a non-negative number!";
+    cin >> num;
+    if (num < 0) {
+        cout << "Error, please try again using non-negative number, or try using a number more than 0!" << endl;
+        return 1;
+    } else {
+        for (int i = 1; i <= num; ++i) {
+            factorial *= i; 
+        }
+        cout << "The factorial of " << num << " is " << factorial << "." << endl;
+    }
+
+    return 0;
+}
